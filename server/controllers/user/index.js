6330ddf80b65e23d5fc39409ec726d7d61d1dbbb
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2019-02-05 17:42:40 +0700

    Configure Apidoc and JSdoc

--- server/controllers/user/index.js
+++ server/controllers/user/index.js
@@ -4,6 +4,18 @@ const router = express.Router();
 const asyncMiddleware = require('../../middlewares/asyncMiddleware');
 const create = require('./user.create');
 
+/**
+ * @api {post} /api/user Create user
+ * @apiName CreateUser
+ * @apiGroup User
+ *
+ * @apiParam {String} firstname Firstname of the user
+ * @apiParam {String} lastname Lastname of the user
+ * @apiParam {String} email Email of the user
+ * @apiParam {String} password Password of the user
+ *
+ * @apiSuccess {String} id id of the created user
+ */
 router.post('/api/user', asyncMiddleware(create));
 
 module.exports = router;
