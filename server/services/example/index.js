2d5fc24cae3c704cc40868c9e6a066cb5aabfa2c
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2019-02-05 17:35:50 +0700

    Check even services & test

--- server/services/example/index.js
+++ server/services/example/index.js
@@ -1,10 +1,22 @@
 const logger = require('../../utils/logger');
 
 module.exports = function ExampleService() {
+  /**
+   * @public
+   * @description This function starts the ExampleService service
+   * @example
+   * gladys.services.example.start();
+   */
   function start() {
     logger.log('starting example service');
   }
 
+  /**
+   * @public
+   * @description This function stops the ExampleService service
+   * @example
+   * gladys.services.example.stop();
+   */
   function stop() {
     logger.log('stopping example service');
   }
