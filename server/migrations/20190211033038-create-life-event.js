cf295cbbd69b101bf7953715bbe57f1292c83e5f
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2019-02-11 10:43:08 +0700

    Create table life_event

---
+++ server/migrations/20190211033038-create-life-event.js
@@ -0,0 +1,36 @@
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('t_life_event', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID,
+      },
+      user_id: {
+        allowNull: false,
+        type: Sequelize.UUID,
+        references: {
+          model: 't_user',
+          key: 'id',
+        },
+      },
+      type: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      data: {
+        type: Sequelize.JSON,
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: (queryInterface, Sequelize) => queryInterface.dropTable('t_life_event'),
+};
---
+++ server/models/life_event.js
@@ -0,0 +1,32 @@
+
+module.exports = (sequelize, DataTypes) => {
+  const lifeEvent = sequelize.define('t_life_event', {
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4,
+    },
+    user_id: {
+      allowNull: false,
+      type: DataTypes.UUID,
+      references: {
+        model: 't_user',
+        key: 'id',
+      },
+    },
+    type: {
+      allowNull: false,
+      type: DataTypes.ENUM(
+        'woke_up',
+        'went_sleeping',
+        'back_at_home',
+        'left_home',
+        'entered_area',
+      ),
+    },
+    data: {
+      type: DataTypes.JSON,
+    },
+  }, {});
+  return lifeEvent;
+};
