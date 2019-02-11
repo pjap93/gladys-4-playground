eb8e4cd89ff70c4db0daf6879cd280e54bf2179a
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2019-02-11 11:32:02 +0700

    Add calendar event

---
+++ server/models/calendar_event.js
@@ -0,0 +1,48 @@
+
+module.exports = (sequelize, DataTypes) => {
+  const calendarEvent = sequelize.define('t_calendar_event', {
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4,
+    },
+    calendar_id: {
+      allowNull: false,
+      type: DataTypes.UUID,
+      references: {
+        model: 't_calendar',
+        key: 'id',
+      },
+    },
+    name: {
+      allowNull: false,
+      unique: true,
+      type: DataTypes.STRING,
+    },
+    selector: {
+      allowNull: false,
+      unique: true,
+      type: DataTypes.STRING,
+    },
+    external_id: {
+      unique: true,
+      type: DataTypes.STRING,
+    },
+    location: {
+      type: DataTypes.STRING,
+    },
+    start: {
+      allowNull: false,
+      type: DataTypes.DATE,
+    },
+    end: {
+      type: DataTypes.DATE,
+    },
+    full_day: {
+      allowNull: false,
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+  }, {});
+  return calendarEvent;
+};
