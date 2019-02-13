ba081ca0a66a5ca31af70b760f051a08d7605dac
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2019-02-13 11:30:45 +0700

    Install services dependencies

---
+++ server/cli/install_service_dependencies.js
@@ -0,0 +1,22 @@
+const { lstatSync, readdirSync } = require('fs');
+const { join } = require('path');
+const { execSync } = require('child_process');
+const logger = require('../utils/logger.js');
+
+const SERVICE_PATH = join(__dirname, '../services');
+
+const isDirectory = source => lstatSync(source).isDirectory();
+const getDirectories = source => readdirSync(source).map(name => join(source, name)).filter(isDirectory);
+
+const directories = getDirectories(SERVICE_PATH);
+
+directories.forEach((directory) => {
+  logger.info(`Installing dependencies in folder ${directory}`);
+  try {
+    execSync(`npm install --prefix ${directory}`, {
+      maxBuffer: 10 * 1000 * 1024, // 10Mo of logs allowed for module with big npm install
+    });
+  } catch (e) {
+    logger.warn(e);
+  }
+});
