14ed8f72c526f315113fc4dfc7af758e4720b01a
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2019-03-26 17:14:53 +0800

    Add more frontend tests to increase coverage

--- front/tests/header.test.js
+++ front/tests/header.test.js
@@ -7,9 +7,13 @@ const fakeUser = {
 };
 
 describe('Initial Test of the Header', () => {
-  test('Header renders Gladys Assistant title and 6 items in header', () => {
+  test('Header renders Gladys Assistant title and 8 items in header', () => {
     const context = shallow(<Header user={fakeUser} currentUrl="/dashboard" />);
     expect(context.find('#header-title').text()).toBe('Gladys Assistant');
     expect(context.find('.nav-item').length).toBe(8);
+  });
+  test('Should return null, login page doesnt need a header', () => {
+    const context = shallow(<Header user={fakeUser} currentUrl="/login" />);
+    expect(context).toBe.null;
   });
 });
