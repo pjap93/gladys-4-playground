04ad7a6488728ce65758a0b902eb170ea57de930
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2019-04-09 11:14:12 +0530

    Add ability to get list of scenes with search and pagination + scene.update

--- server/lib/scene/index.js
+++ server/lib/scene/index.js
@@ -2,7 +2,9 @@ const queue = require('queue');
 const { addScene } = require('./scene.addScene');
 const { create } = require('./scene.create');
 const { execute } = require('./scene.execute');
+const { get } = require('./scene.get');
 const { executeSingleAction } = require('./scene.executeSingleAction');
+const { update } = require('./scene.update');
 
 const SceneManager = function SceneManager(stateManager) {
   this.stateManager = stateManager;
@@ -16,7 +18,9 @@ const SceneManager = function SceneManager(stateManager) {
 
 SceneManager.prototype.addScene = addScene;
 SceneManager.prototype.create = create;
+SceneManager.prototype.get = get;
 SceneManager.prototype.execute = execute;
 SceneManager.prototype.executeSingleAction = executeSingleAction;
+SceneManager.prototype.update = update;
 
 module.exports = SceneManager;
