89c5b8ae9926957cb89970a2254c1002e3c42020
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2019-04-23 09:25:30 +0530

    room.getByName case insensitive

--- server/lib/room/index.js
+++ server/lib/room/index.js
@@ -1,10 +1,12 @@
 const { create } = require('./room.create');
+const { getByName } = require('./room.getByName');
 const { update } = require('./room.update');
 const { destroy } = require('./room.destroy');
 
 const Room = function Room() {};
 
 Room.prototype.create = create;
+Room.prototype.getByName = getByName;
 Room.prototype.update = update;
 Room.prototype.destroy = destroy;
 
--- server/test/lib/room/room.test.js
+++ server/test/lib/room/room.test.js
@@ -47,3 +47,33 @@ describe('room.destroy', () => {
     return assert.isRejected(promise, 'Room not found');
   });
 });
+
+describe('room.getByName', () => {
+  const room = new Room();
+  it('should get a room by name', async () => {
+    const roomFound = await room.getByName('Test room');
+    expect(roomFound).to.deep.equal({
+      id: '2398c689-8b47-43cc-ad32-e98d9be098b5',
+      house_id: 'a741dfa6-24de-4b46-afc7-370772f068d5',
+      name: 'Test room',
+      selector: 'test-room',
+      created_at: new Date('2019-02-12T07:49:07.556Z'),
+      updated_at: new Date('2019-02-12T07:49:07.556Z'),
+    });
+  });
+  it('should get a room by name, case insensitive', async () => {
+    const roomFound = await room.getByName('test ROOM');
+    expect(roomFound).to.deep.equal({
+      id: '2398c689-8b47-43cc-ad32-e98d9be098b5',
+      house_id: 'a741dfa6-24de-4b46-afc7-370772f068d5',
+      name: 'Test room',
+      selector: 'test-room',
+      created_at: new Date('2019-02-12T07:49:07.556Z'),
+      updated_at: new Date('2019-02-12T07:49:07.556Z'),
+    });
+  });
+  it('should return room not found', async () => {
+    const promise = room.getByName('Room does not exist');
+    return assert.isRejected(promise, 'Room not found');
+  });
+});
